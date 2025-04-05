91d95e1fb4e9cdc3467a344ce53f2c0b377b243c
ElengPregunts28 <ellainepregunta28@gmail.com>
2025-04-05 13:44:11 +0800

    Done. Midterm Exam.

--- 02_fs.js
+++ 02_fs.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 fs.writeFile("message.txt", 'Node.js is awesome!', (err) => {
   if (err) {
     console.error("Error writing to file:", err);
-    return;
   }
 
   console.log("File created and data written successfully.");
@@ -11,7 +10,6 @@ fs.writeFile("message.txt", 'Node.js is awesome!', (err) => {
   fs.readFile('message.txt', 'utf8', (err, data) => {
     if (err) {
       console.error("Error reading the file:", err);
-      return;
     }
     console.log("File contents:", data);
 
@@ -19,7 +17,6 @@ fs.writeFile("message.txt", 'Node.js is awesome!', (err) => {
     fs.unlink("message.txt", (err) => {
       if (err) {
         console.error("Error deleting the file:", err);
-        return;
       }
       console.log("File deleted successfully.");
     });
